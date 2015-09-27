e74ed15ae7591d2bde6061a0a8abdbba9dd930f9
Marko Haanranta <marko.haanranta@helsinki.fi>
2015-09-27 16:33:32 +0300

    remove, moveup, movedown

--- app.js
+++ app.js
@@ -7,7 +7,42 @@ function MyApp() {
         $("#app>footer").text(message);
     }
     
+    function addTask() {
+        var taskName=$("#new-task-name").val();
+        if(taskName) {
+            addTaskElement(taskName);
+            $("#new-task-name").val("").focus();
+        }
+    }
+    
+    function addTaskElement(taskName) {
+        var $task = $("<li></li>");
+        var $delete=$("<button class='delete'>X</button>");
+        var $moveUp = $("<button class='move-up'>^</button>");
+        var $moveDown = $("<button class='move-down'>v</button>");
+        $task.append($delete)
+             .append($moveUp)
+             .append($moveDown)
+             .append("<span class='task-name'>"+taskName+"</span>");
+        $("#task-list").append($task);
+        
+        $delete.click(function() {$task.remove();});
+        $moveUp.click(function() {
+           $task.insertBefore($task.prev()); 
+        });
+        $moveDown.click(function() {
+           $task.insertAfter($task.next()); 
+        });
+       
+    }
+    
     this.start = function() {
+        $("#new-task-name").keypress(function(e) {
+            if(e.which===13) {
+                addTask();
+                return false;
+            }
+        });
         $("#app>header").append(version);
         setStatus("ready");
     };
