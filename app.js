100c19acfd0cf8befc241e8e76b0ac578ad0ec82
Marko Haanranta <marko.haanranta@helsinki.fi>
2015-10-04 16:36:19 +0300

    template added

--- app.js
+++ app.js
@@ -16,21 +16,17 @@ function MyApp() {
     }
     
     function addTaskElement(taskName) {
-        var $task = $("<li></li>");
-        var $delete=$("<button class='delete'>X</button>");
-        var $moveUp = $("<button class='move-up'>^</button>");
-        var $moveDown = $("<button class='move-down'>v</button>");
-        $task.append($delete)
-             .append($moveUp)
-             .append($moveDown)
-             .append("<span class='task-name'>"+taskName+"</span>");
+        var $task = $("#task-template .task").clone();
+        $("span.task-name", $task).text(taskName);
         $("#task-list").append($task);
         
-        $delete.click(function() {$task.remove();});
-        $moveUp.click(function() {
+        $("button.delete", $task).click(function() {
+            $task.remove();
+        });
+        $("button.move-up", $task).click(function() {
            $task.insertBefore($task.prev()); 
         });
-        $moveDown.click(function() {
+        $("button.move-down").click(function() {
            $task.insertAfter($task.next()); 
         });
        
