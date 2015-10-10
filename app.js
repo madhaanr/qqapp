7a4f10bcc3a3e22bfb35de39c209a07a9dfc47ea
Marko Haanranta <marko.haanranta@helsinki.fi>
2015-10-10 18:16:30 +0300

    v3.2

--- app.js
+++ app.js
@@ -1,67 +1,89 @@
 "use strict";
 
 function MyApp() {
-    var version="v3.1",
-        appStorage=new AppStorage("taskAtHand");
-    
+    var version = "v3.2",
+            appStorage = new AppStorage("taskAtHand"),
+            taskList = new TaskList();
+
     function setStatus(message) {
         $("#app>footer").text(message);
     }
-    
+
     function addTask() {
-        var taskName=$("#new-task-name").val();
-        if(taskName) {
-            addTaskElement(taskName);
+        var taskName = $("#new-task-name").val();
+        if (taskName) {
+            var task = new Task(taskName);
+            taskList.addTask(task);
+            appStorage.setValue("nextTaskId", Task.nextTaskId);
+            addTaskElement(task);
             saveTaskList();
             $("#new-task-name").val("").focus();
         }
     }
-    
-    function addTaskElement(taskName) {
+
+    function addTaskElement(task) {
         var $task = $("#task-template .task").clone();
-        $task.click(function() {
-           onSelectTask($task); 
+        $task.data("task-id", task.id);
+        $("span.task-name", $task).text(task.name);
+        $(".details input, .details select", $task).each(function () {
+            var $input = $(this);
+            var fieldName = $input.data("field");
+            $input.val(task[fieldName]);
         });
-        $("span.task-name", $task).text(taskName);
-        
         $("#task-list").append($task);
-        
-        $("button.delete", $task).click(function() {
+
+        $("button.delete", $task).click(function () {
             removeTask($task, true);
         });
-        $("button.move-up", $task).click(function() {
-           moveTask($task,true); 
+        $("button.move-up", $task).click(function () {
+            moveTask($task, true);
         });
-        $("button.move-down",$task).click(function() {
-           moveTask($task,false); 
+        $("button.move-down", $task).click(function () {
+            moveTask($task, false);
         });
-        $("span.task-name",$task).click(function() {
-           onEditTaskName($(this)); 
+        $("span.task-name", $task).click(function () {
+            onEditTaskName($(this));
         });
-        $("input.task-name",$task).change(function() {
-           onChangeTaskName($(this)); 
-        }).blur(function() {
-           $(this).hide().siblings("span.task-name").show(); 
+        $("input.task-name", $task).change(function () {
+            onChangeTaskName($(this));
+        }).blur(function () {
+            $(this).hide().siblings("span.task-name").show();
         });
-        $("button.toggle-details",$task).click(function() {
-        toggleDetails($task);
-    });
+        $(".details input, .details select",$task).change((function() {
+            onChangeTaskDetails(task.id,$(this));
+        }))
+        
+        $("button.toggle-details", $task).click(function () {
+            $task.click(function () {
+                onSelectTask($task);
+            });
+            toggleDetails($task);
+        });
     }
     
+    function onChangeTaskDetails(taskId, $input) {
+        var task = taskList.getTask(taskId);
+        if(task) {
+            var fieldName=$input.data("field");
+            task[fieldName]=$input.val();
+            saveTaskList();
+        }
+    }
+
     function onSelectTask($task) {
-        if($task) {
+        if ($task) {
             $task.siblings(".selected").removeClass("selected");
             $task.addClass("selected");
         }
     }
-    
+
     function removeTask($task) {
         $task.remove();
         saveTaskList();
     }
-    
+
     function moveTask($task, moveUp) {
-        if(moveUp) {
+        if (moveUp) {
             $task.insertBefore($task.prev());
         }
         else {
@@ -69,68 +91,68 @@ function MyApp() {
         }
         saveTaskList();
     }
-    
+
     function onEditTaskName($span) {
         $span.hide()
-             .siblings("input.task-name")
-             .val($span.text())
-             .show()
-             .focus();
+                .siblings("input.task-name")
+                .val($span.text())
+                .show()
+                .focus();
     }
-    
+
     function onChangeTaskName($input) {
         $input.hide();
-        var $span=$input.siblings("span.task-name");
-        if($input.val()) {
+        var $span = $input.siblings("span.task-name");
+        if ($input.val()) {
             $span.text($input.val());
         }
         saveTaskList();
         $span.show();
     }
-    
+
     function saveTaskList() {
-        var tasks=[];
-        $("#task-list .task span.task-name").each(function() {
-            tasks.push($(this).text());
-        });
-        appStorage.setValue("taskList",tasks);
+        appStorage.setValue("taskList",taskList.getTasks());
     }
-    
+
     function loadTaskList() {
-        var tasks=appStorage.getValue('taskList');
-        if(tasks) {
-            for (var i in tasks) {
-                addTaskElement(tasks[i]);
-            }
-        }
+        var tasks = appStorage.getValue("taskList");
+        taskList = new TaskList(tasks);
+        rebuildTaskList();
     }
     
+    function rebuildTaskList() {
+        $("#task-list").empty();
+        taskList.each(function(task) {
+           addTaskElement(task); 
+        });
+    }
+
     function onChangeTheme() {
         var theme = $("#theme>option").filter(":selected").val();
         setTheme(theme);
-        appStorage.setValue("theme",theme);
+        appStorage.setValue("theme", theme);
     }
-    
+
     function setTheme(theme) {
-        $("#theme-style").attr("href","themes/"+theme+".css");
+        $("#theme-style").attr("href", "themes/" + theme + ".css");
     }
     function loadTheme() {
-        var theme=appStorage.getValue("theme");
-        if(theme) {
+        var theme = appStorage.getValue("theme");
+        if (theme) {
             setTheme(theme);
-            $("#theme>option[value="+theme+"]")
-            .attr("selected","selected");
+            $("#theme>option[value=" + theme + "]")
+                    .attr("selected", "selected");
         }
-    } 
-    
-    function toggleDetails($task) {
-        $(".details",$task).slideToggle();
-        $("button.toggle-details",$task).toggleClass("expanded");
     }
-    
-    this.start = function() {
-        $("#new-task-name").keypress(function(e) {
-            if(e.which===13) {
+
+    function toggleDetails($task) {
+        $(".details", $task).slideToggle();
+        $("button.toggle-details", $task).toggleClass("expanded");
+    }
+
+    this.start = function () {
+        $("#new-task-name").keypress(function (e) {
+            if (e.which === 13) {
                 addTask();
                 return false;
             }
@@ -143,7 +165,7 @@ function MyApp() {
     };
 }
 
-$(function() {
+$(function () {
     window.app = new MyApp();
     window.app.start();
 });
